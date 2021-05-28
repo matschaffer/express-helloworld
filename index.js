270d123567f1d73a397aefaae8c5f9ca09497e68
Mat Schaffer <mat@schaffer.me>
2021-05-28 07:08:12 +0000

    Try an update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send("Hello from Mat's World!")
+  res.send("Hello from Mat's (updated) World!")
 })
 
 app.listen(port, () => {
